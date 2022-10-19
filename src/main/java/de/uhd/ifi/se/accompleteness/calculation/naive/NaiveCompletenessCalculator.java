ef8b9abef49a1fe505e16417232ac7699f2fe725
SIoop <alexander.lepper@t-online.de>
2022-10-19 16:34:40 +0200

    update naive completeness calculation

--- src/main/java/de/uhd/ifi/se/accompleteness/calculation/naive/NaiveCompletenessCalculator.java
+++ src/main/java/de/uhd/ifi/se/accompleteness/calculation/naive/NaiveCompletenessCalculator.java
@@ -4,19 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import de.uhd.ifi.se.accompleteness.calculation.CompletenessCalculator;
+import de.uhd.ifi.se.accompleteness.model.CompletenessCalcResult;
 import de.uhd.ifi.se.accompleteness.model.NLPResultSingle;
-import de.uhd.ifi.se.accompleteness.model.Relationship;
 import de.uhd.ifi.se.accompleteness.model.Topic;
 
 public class NaiveCompletenessCalculator implements CompletenessCalculator {
 
     @Override
-    public Map<String, Double> calculate_completeness(NLPResultSingle usResult, NLPResultSingle acResult) {
+    public CompletenessCalcResult calculate_completeness(NLPResultSingle usResult, NLPResultSingle acResult) {
         Map<String, Double> toReturn = new HashMap<>();
+        Map<Topic, Topic> matchedTopics = new HashMap<>();
         int topicsFound = 0;
         int topicsAll = 0;
-        int relationshipsFound = 0;
-        int relationshipsAll = 0;
         for (Topic topic: usResult.getTopics()) {
             boolean topicFound = false;
             for (Topic topic2 : acResult.getTopics()) {
@@ -29,20 +28,8 @@ public class NaiveCompletenessCalculator implements CompletenessCalculator {
             } 
             topicsAll++;
         }
-        for (Relationship relationship: usResult.getRelationships()) {
-            boolean relationshipFound = false;
-            for (Relationship relationship2: acResult.getRelationships()) {
-                if (relationship == relationship2) {
-                    relationshipFound = true;
-                }
-            }
-            if (relationshipFound) {
-                relationshipsFound++;
-            }
-            relationshipsAll++;
-        }
         toReturn.put("completeness", ((double)(topicsFound) / (double)(topicsAll)));
-        return toReturn;
+        return new CompletenessCalcResult(toReturn, matchedTopics);
     }
     
 }
